7a1b53b08addaafd14d374310fd48c90c6c2fbb7
Mohammed Thanveer <103193237+Thanveerkalathil@users.noreply.github.com>
2024-08-16 18:37:25 +0530

    secured

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,13 +4,13 @@ import {getFirestore} from '@firebase/firestore'
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDqBXswWm3YaZpzkx6e8XAybK343SWWk5k",
-    authDomain: "task-manager-931cc.firebaseapp.com",
-    projectId: "task-manager-931cc",
-    storageBucket: "task-manager-931cc.appspot.com",
-    messagingSenderId: "546543860137",
-    appId: "1:546543860137:web:f34369f4bb80478f286cb8",
-    measurementId: "G-TZ8SK74H41"
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
+    measurementId:REACT_APP_FIREBASE_MEASUREMENT_ID
   };
   const app = initializeApp(firebaseConfig);
   export const db = getFirestore(app);
