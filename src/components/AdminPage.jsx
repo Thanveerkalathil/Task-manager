5e8f779d1ae1073250e4a9b67da6d18813280c7c
Mohammed Thanveer <103193237+Thanveerkalathil@users.noreply.github.com>
2024-08-17 12:57:50 +0530

    admin userView

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,35 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
+import { db } from "../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 const AddUserPage = () => {
   const [users, setUsers] = useState([]);
-  const [newUser, setNewUser] = useState({ username: '', email: '', password: 'defaultPassword' });
-  const [error, setError] = useState('');
+  const [newUser, setNewUser] = useState({
+    username: "",
+    email: "",
+    password: "defaultPassword",
+  });
+  const [error, setError] = useState("");
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setNewUser({ ...newUser, [name]: value });
   };
 
-  const userstemp = [
-    { username: 'amal', email: 'amal@gmail.com' },
-    { username: 'thanveer', email: 'thanveer@gmail.com' },
-    { username: 'hisham', email: 'hisham@gmail.com' }
-  ];
+  const userCollectionRef = collection(db, "users");
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(userCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getUsers();
+  }, [users]);
 
   const handleAddUser = (e) => {
     e.preventDefault();
     if (newUser.username && newUser.email) {
       setUsers([...users, { id: Date.now(), ...newUser }]);
-      setNewUser({ username: '', email: '', password: 'defaultPassword' });
-      setError('');
+      setNewUser({ username: "", email: "", password: "defaultPassword" });
+      setError("");
     } else {
-      setError('Username and Email are required.');
+      setError("Username and Email are required.");
     }
   };
 
   const handleDeleteUser = (id) => {
-    setUsers(users.filter(user => user.id !== id));
+    setUsers(users.filter((user) => user.id !== id));
   };
 
   return (
@@ -37,7 +46,7 @@ const AddUserPage = () => {
       <Header />
       <div className="flex flex-wrap justify-center items-start flex-1 bg-gray-200 px-4 md:px-16 py-8 gap-8 overflow-hidden">
         {/* Add Button */}
-        <div className="w-full md:w-1/2 lg:w-1/3 bg-white p-6 rounded-lg shadow-lg border border-none">
+        <div className="w-full h-96 md:w-1/2 lg:w-1/3 bg-white p-6 rounded-lg shadow-lg border border-none">
           <h2 className="text-3xl text-center font-bold text-black mb-6 font-mono">
             Add User
           </h2>
@@ -88,18 +97,20 @@ const AddUserPage = () => {
           </form>
         </div>
         {/* View Users Section */}
-        <div className="w-full md:w-1/2 lg:w-1/3 bg-white p-6 rounded-lg shadow-lg border border-none">
+        <div className="w-full h-96 md:w-1/2 lg:w-1/3 bg-white p-6 rounded-lg shadow-lg border border-none">
           <h2 className="text-3xl text-center font-semibold text-black mb-6 font-mono">
             View Users
           </h2>
           <ul className="bg-white rounded-lg shadow-lg border border-none">
-            {userstemp.map((user) => (
+            {users.map((user) => (
               <li
                 key={user.id}
                 className="p-4 border-b last:border-b-0 border-gray-300 flex justify-between items-center"
               >
                 <div>
-                  <p className="text-black font-medium font-mono">{user.username}</p>
+                  <p className="text-black font-medium font-mono">
+                    {user.username}
+                  </p>
                   <p className="text-black font-mono">{user.email}</p>
                 </div>
                 <button
