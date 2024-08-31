241d6b5a050b31127ff8333b18b8fd9f65a9f447
Amal Muhammed <amalmn32@gmail.com>
2024-08-31 17:12:14 +0530

    backend env vars

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
-const admin = require('firebase-admin');
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const path = require('path'); 
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 // Initialize Firebase Admin SDK
-const serviceAccount = path.resolve(__dirname, process.env.FIREBASE_ADMIN_SDK_KEY_PATH);
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,10 +24,10 @@ admin.initializeApp({
 // Express setup (if you're using Express)
 const app = express();
 app.use(express.json());
-app.use(cors());// Use the CORS middleware
+app.use(cors()); // Use the CORS middleware
 
 // Endpoint to create a new user
-app.post('/create-user', async (req, res) => {
+app.post("/create-user", async (req, res) => {
   const { email, password, username } = req.body;
   try {
     const userRecord = await admin.auth().createUser({
